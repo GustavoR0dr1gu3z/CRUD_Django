2e3189455c31624d1a4ee1fb494dd1bfa3a1590c
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-04 20:21:55 -0600

    creando el modelo para el contacto

--- Django_AdressBook/Contact/models.py
+++ Django_AdressBook/Contact/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model): # Creo clase e importo los modelos
+    first_name = models.CharField(max_length=50)    
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(blank=True) # Email que acepta nulos
+    
