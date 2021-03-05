6abd71526650ba2bab1c56029b7e3c23bf56f8ee
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-04 22:23:32 -0600

    arreglando las urls

--- Django_AdressBook/Contact/urls.py
+++ Django_AdressBook/Contact/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('',ContactListView.as_view(), name="contactList"),
     path('addContact/',ContactCreateView.as_view(), name="createContact"),
     path('detail/<int:pk>', ContactDetailView.as_view() ,name="contact_detail"),
-    path('edit/<int>:pk', ContactUpdateView.as_view(), name="contact_update"),
-    path('detelete/<int>:pk', ContactDeleteView.as_view(), name="contact_delete"),
+    path('edit/<int:pk>', ContactUpdateView.as_view(), name="contact_update"),
+    path('delete/<int:pk>', ContactDeleteView.as_view(), name="contact_delete"),
 ]
